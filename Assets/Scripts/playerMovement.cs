60eae246ffd92584c6bbd2ca7c087999bb9548b5
panagiotisnikos <panagiotis.nikos@hotmail.com>
2025-11-03 23:41:39 +0200

    updated player movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,27 @@ public class playerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     void Start()
     {
-        rb.useGravity = false;
+        //rb.useGravity = false;
+        rb.AddForce(0, 0, forwardForce); // (x,y,z)
         
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 10 * Time.deltaTime); // (x,y,z)
+        //αυτό το ξεχνάμε
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+       if (Input.GetKey("a"))
+         {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
     }
 }
